9c1e1fc458de797f78cec86c3a7c2c3fbc77f209
Yegor Maksymchuk <ymaksymc@redhat.com>
2018-11-23 23:05:08 +0200

    Initial commit

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Demo for XP Days 2018
 (link)[]
 
 #### If you want to run a test:
+* ``git clone https://github.com/YegorMaksymchuk/XPDays-2018.git``
 * install [maven](https://maven.apache.org/) and [java](https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
 * install [openshift origin](https://github.com/openshift/origin) now it call [okd](https://www.okd.io/download.html) 
 * oc cluster up
--- src/test/java/online/qastudy/okd/demo/deployment/DemoDeploymetnTest.java
+++ src/test/java/online/qastudy/okd/demo/deployment/DemoDeploymetnTest.java
@@ -8,42 +8,40 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.util.concurrent.TimeUnit;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class DemoDeploymetnTest {
     private DemoDeployment demoDeployment;
-        private WebDriver driver;
+    private WebDriver driver;
 
-        @Before
-        public void deploy() {
-            System.setProperty("webdriver.chrome.driver","/home/ymaks/sources/IdeaProjects/CODE/XPDays-2018/src/test/resources/chromedriver");
-            driver = new ChromeDriver();
-            driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
-            driver.manage().window().maximize();
+    @Before
+    public void deploy() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+        demoDeployment = new DemoDeployment("xpdays-2018");
+    }
 
-            demoDeployment = new DemoDeployment("xpdays-2018");
-        }
+    @After
+    public void cleanup() {
+        demoDeployment.close();
+    }
 
-        @After
-        public void cleanup() {
-            demoDeployment.close();
-        }
+    @Test
+    public void testAppDeployment() {
+        demoDeployment.login()
+                .createNewProject("xpdays-2018", "Demo for XPDays 2018", "Demo of Fabric8")
+                .deployPod()
+                .deployService()
+                .createRout();
 
-        @Test
-        public void testAppDeployment() {
+        driver.navigate().to(demoDeployment.getApplicationURL());
+        Util.WaitUntilAppWillBeReady();
+        driver.navigate().refresh();
 
-            demoDeployment.login()
-                    .createNewProject("xpdays-2018", "Demo for XPDays 2018", "Demo of Fabric8")
-                    .deployPod()
-                    .deployService()
-                    .createRout();
-
-            driver.navigate().to(demoDeployment.getApplicationURL());
-            Util.WaitUntilAppWillBeReady();
-            driver.navigate().refresh();
-
-            assertThat(driver.findElement(By.className("center")).getText()).contains("Hello from POD!!");
-        }
+        assertThat(driver.findElement(By.className("center")).getText()).contains("Hello from POD!!");
+    }
 }
